a46c7b7227cf76f2a05eda8fda16b09b07f96f87
Cesar <cesdia883@gmail.com>
2021-01-26 18:45:10 -0500

    login page render and base for registration, db chosen

--- routes/authentication/index.js
+++ routes/authentication/index.js
@@ -1,10 +1,43 @@
 const authRouter = require("express").Router();
 
 authRouter
-    .route("/")
+    /* Login Page */
+    .route("/login")
+
     // GET Login Page
     .get((req, res, next) => {
-        res.render("login", { pageTitle: "Login"});
+        res.render("login", { pageTitle: "Login" });
+    })
+
+    // POST Login Logic
+    .post(async (req, res, next) => {
+        try {
+            
+            res.send("Success Logging In") // TODO: Success Logic
+        } catch (error) {
+            if (error instanceof AuthenticationError) {
+                res.render("login", {
+                    pageTitle: "Login",
+                    Errors: error.message,
+                });
+            } else {
+                next(error);
+            }
+        }
+    })
+
+    /* Register Page */
+    .route("/register")
+
+    // GET Register PAge
+    .get((req, res, next) => {
+        res.render("register", { pageTitle: "Register" });
+    })
+
+    // POST Register Page
+    .post(async (req, res, next) => {
+        try {
+        } catch (error) {}
     });
 
 module.exports = authRouter;
---
+++ utils/errorHandler.js
@@ -0,0 +1,17 @@
+export class AuthenticationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "Authentication Error";
+    }
+}
+
+// UserName 
+// Password
+// 
+// Exercise 
+// weight
+// Reps
+// DateTime
+// 
+// Goal
+// Notes
